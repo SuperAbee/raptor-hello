79f72c0fc843383aedeaa49d5dbad159ad2c33a8
SuperAbee <yaoxincong@outlook.com>
2022-04-19 14:10:08 +0800

    fix

--- main.go
+++ main.go
@@ -103,6 +103,6 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	time.Sleep(time.Duration(n) * time.Second)
 	_, _ = fmt.Fprintf(w, "Hello")
-	_, _ = fmt.Println("Hello")
+	log.Println("Hello")
 }
 
